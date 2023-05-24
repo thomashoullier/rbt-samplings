0c63df15648c58beb86ef3ce3426a7905bd1a4e3
Thomas HOULLIER <pro@houllier.net>
2023-05-24 20:47:04 +0200

    filled the example file with ray bundle conversion and triangulation

--- example.cpp
+++ example.cpp
@@ -12,4 +12,12 @@ int main () {
   std::cout << "## disk_uniform sampling" << std::endl;
   disk_uniform du(3);
   std::cout << du << std::endl;
+
+  std::cout << "## Convert to a ray bundle" << std::endl;
+  bun du_b = du.to_ray_bundle(Vec3(0, 0, 1));
+  std::cout << du_b << std::endl;
+
+  std::cout << "## Triangulate the sampling" << std::endl;
+  triangulation du_tri = du.triangulate();
+  std::cout << du_tri << std::endl;
 }
